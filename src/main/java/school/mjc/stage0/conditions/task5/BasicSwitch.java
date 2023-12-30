50dd98559ad478899e29332f634ecd74932f5e70
faridabderrahmani <abderrahmani@yandex.ru>
2023-12-30 13:38:25 +0100

    resolved

--- src/main/java/school/mjc/stage0/conditions/task5/BasicSwitch.java
+++ src/main/java/school/mjc/stage0/conditions/task5/BasicSwitch.java
@@ -1,8 +1,10 @@
 package school.mjc.stage0.conditions.task5;
 
+
 public class BasicSwitch {
+
     public static void main(String[] args) {
-        int a = 20;
+        int a = 0;
         switch (a) {
             default:
                 System.out.println("default");
